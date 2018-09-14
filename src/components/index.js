d8f1b7db872731e06064ae66ecc008472057d0fb
Felix Wu <mail@flxwu.com>
2018-09-14 11:26:38 +0200

    export lists and misc

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import * as Headers from './headers';
 import * as Emphasis from './emphasis';
+import * as Lists from './lists';
+import * as Misc from './misc';
 
 export default {
   Headers,
   Emphasis,
+  Lists,
+  Misc
 };
