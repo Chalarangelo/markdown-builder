a1269d53734d7c321dafc10907597f8c65266c14
Felix Wu <mail@flxwu.com>
2018-09-14 11:26:18 +0200

    add miscellaneous

---
+++ src/components/misc.js
@@ -0,0 +1,45 @@
+/**
+ * Misc
+ * @flow
+ */
+
+import {
+  HORIZONTAL_RULE,
+  SECTION_LINE_BREAK,
+  surround,
+  LINE_BREAK
+} from '../util';
+
+const hr = () => surround(SECTION_LINE_BREAK, HORIZONTAL_RULE);
+
+const collapsible = (summary: string, content: string) =>
+  SECTION_LINE_BREAK +
+  `
+<details>
+  <summary>${summary}</summary>
+  ${content}
+</details>
+` +
+  LINE_BREAK;
+
+const anchor = val =>
+  val
+    .trim()
+    .toLowerCase()
+    .replace(/[^\w\- ]+/g, '')
+    .replace(/\s/g, '-')
+    .replace(/\-+$/, '');
+
+const link = (title: string, url: string = null) => {
+  if (url === null) {
+    url = anchor(title);
+  }
+  return `[${title}](${url})`;
+};
+
+export {
+  hr,
+  collapsible,
+  anchor,
+  link
+};
