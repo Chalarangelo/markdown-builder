9f06eaa507d5f23073d350a5cb56d08b15b1fde7
Felix Wu <mail@flxwu.com>
2018-09-14 12:34:13 +0200

    paranthesis typo

--- src/components/misc.js
+++ src/components/misc.js
@@ -38,7 +38,7 @@ const link = (title: string, url: string|null = null) => {
 };
 
 const image = (alt:string, url:string, title: string = '') =>
-  `![${alt}](${url} "${title}"})`
+  `![${alt}](${url} "${title}")`
 
 export {
   hr,
