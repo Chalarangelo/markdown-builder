e89292f0c5a9b9edcc9cf996bc7c1b7df20c280a
Felix Wu <mail@flxwu.com>
2018-09-14 11:34:49 +0200

    adapt headers test

--- __test__/components/headers.spec.js
+++ __test__/components/headers.spec.js
@@ -2,11 +2,11 @@ const components = require('../../lib/components').default;
 
 const Headers = components.Headers;
 test('Header builders', () => {
-  expect(Headers.default(1, 'H1 header')).toBe('# H1 header'); // hX
-  expect(Headers.h1('H1 header')).toBe('# H1 header');
-  expect(Headers.h2('H2 header')).toBe('## H2 header');
-  expect(Headers.h3('H3 header')).toBe('### H3 header');
-  expect(Headers.h4('H4 header')).toBe('#### H4 header');
-  expect(Headers.h5('H5 header')).toBe('##### H5 header');
-  expect(Headers.h6('H6 header')).toBe('###### H6 header');
+  expect(Headers.default(1, 'H1 header')).toBe('  \n# H1 header'); // hX
+  expect(Headers.h1('H1 header')).toBe('  \n# H1 header');
+  expect(Headers.h2('H2 header')).toBe('  \n## H2 header');
+  expect(Headers.h3('H3 header')).toBe('  \n### H3 header');
+  expect(Headers.h4('H4 header')).toBe('  \n#### H4 header');
+  expect(Headers.h5('H5 header')).toBe('  \n##### H5 header');
+  expect(Headers.h6('H6 header')).toBe('  \n###### H6 header');
 });
